528d67bf8a731ca60b28009197a0b76dd61069fa
Mengyu <mhan8@slb.com>
2022-10-19 17:51:38 +0800

    feat: Add http service.

--- ExpenseRecord/ClientApp/src/app/service/expense.service.ts
+++ ExpenseRecord/ClientApp/src/app/service/expense.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TaggedTemplateExpr } from '@angular/compiler';
+import { Inject, Injectable } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
+import { Expense } from '../models/expense.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpenseService {
+  private baseUrl: string;
+  private http: HttpClient;
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) { 
+    this.http = http;
+    this.baseUrl = baseUrl;
+  }
 
-  constructor() { }
+  getExpenseList(): Observable<Expense[]>{
+    return this.http.get<Expense[]>(this.baseUrl + 'expenses', { responseType: 'text' as 'json' })
+    .pipe(
+      map(res => res as Expense[])
+    );
+  }
+
+  addExpense(expense: Expense): Observable<boolean> {
+    return this.http.post<number>(this.baseUrl + 'expenses', { responseType: 'text' as 'json', body: expense })
+    .pipe(
+      map( res => res === 200)
+    );
+  }
+
+  deleteExpense(expense: Expense): Observable<boolean> {
+    return this.http.post<number>(this.baseUrl + 'expenses', { responseType: 'text' as 'json',  body: expense})
+    .pipe(
+      map( res => res === 200)
+    );
+  }
 }
